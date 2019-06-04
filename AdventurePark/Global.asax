18d397c1f0d60f20e4ad9af364f99b77bc814a3b
Opilane TTHK <opilane@tthk.ee>
2019-06-04 08:52:21 +0300

    Add project files.

---
+++ AdventurePark/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdventurePark.MvcApplication" Language="C#" %>
---
+++ AdventurePark/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdventurePark.Startup))]
+namespace AdventurePark
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
